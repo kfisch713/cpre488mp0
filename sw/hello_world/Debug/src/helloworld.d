2086978d08a8987a0ba3016f3d4e82febd5b9a2b
Fischer <kfisch13@iastate.edu>
2017-01-19 18:57:48 -0600

    end of lab 1/19

---
+++ sw/hello_world/Debug/src/helloworld.d
@@ -0,0 +1,12 @@
+src/helloworld.d: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../system_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../system_bsp/ps7_cortexa9_0/include/xparameters_ps.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../system_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../system_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
---
+++ sw/hello_world/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.d: ../src/platform.c \
+ ../../system_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../system_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../system_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../system_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../system_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../system_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../system_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../system_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
